d155ffd66003dacf269e755a38860becc9e6fc14
HighRadiation <bugraoksuz6@gmail.com>
2025-09-11 18:30:43 +0300

    get-next-line-bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: boksuz <boksuz@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 17:32:26 by boksuz            #+#    #+#             */
+/*   Updated: 2025/09/11 17:32:56 by boksuz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 61
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *str);
+char	*gnl_read(int fd, char *stash);
+char	*gnl_get_line(char *stash);
+char	*gnl_trim_stash(char *stash);
+
+size_t	ft_strlen(const char *str);
+
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+
+#endif
